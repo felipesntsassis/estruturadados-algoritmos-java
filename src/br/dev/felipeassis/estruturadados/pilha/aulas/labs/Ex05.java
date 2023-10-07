b39dd23d40c9a366426e367343f8e99bec998ae1
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-10-07 12:15:41 -0300

    Implementação do exercício 5: Desafío Palíndromo usando pilhas.

---
+++ src/br/dev/felipeassis/estruturadados/pilha/aulas/labs/Ex05.java
@@ -0,0 +1,42 @@
+package br.dev.felipeassis.estruturadados.pilha.aulas.labs;
+
+import br.dev.felipeassis.estruturadados.pilha.Pilha;
+
+public class Ex05 {
+
+    public static void main(String[] args) {
+        imprimeResultado("ada");
+        imprimeResultado("abcd");
+        imprimeResultado("abccba");
+        imprimeResultado("maria");
+    }
+
+    public static void imprimeResultado(String palavra) {
+        System.out.println(palavra + " é um palíndromo? " + testaPalindromo(palavra));
+    }
+
+    public static boolean testaPalindromo(String palavra) {
+        Pilha<Character> pilha = new Pilha<>();
+        // ADA
+        // [A]
+        // [D]
+        // [A]
+
+        for (int i = 0; i < palavra.length(); i ++) {
+            pilha.empilha(palavra.charAt(i));
+        }
+
+        String palavraInversa = "";
+
+        while(!pilha.estaVazia()) {
+            palavraInversa += pilha.desempilha();
+        }
+
+        if (palavraInversa.equalsIgnoreCase(palavra)) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
