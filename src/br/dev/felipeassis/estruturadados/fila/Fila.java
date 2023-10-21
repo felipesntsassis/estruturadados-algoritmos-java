d8e55b6cfea8429746a459bf66a475cd15768186
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-10-21 09:53:51 -0300

    Implementação da aula 19.

---
+++ src/br/dev/felipeassis/estruturadados/fila/Fila.java
@@ -0,0 +1,15 @@
+package br.dev.felipeassis.estruturadados.fila;
+
+import br.dev.felipeassis.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+}
