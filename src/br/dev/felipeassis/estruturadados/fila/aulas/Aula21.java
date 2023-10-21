a8cabdbc5222d8142225ffc542f2d5b50851cdd9
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-10-21 10:06:39 -0300

    Implementação da Aula 21.

---
+++ src/br/dev/felipeassis/estruturadados/fila/aulas/Aula21.java
@@ -0,0 +1,15 @@
+package br.dev.felipeassis.estruturadados.fila.aulas;
+
+import br.dev.felipeassis.estruturadados.fila.Fila;
+
+public class Aula21 {
+
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+        fila.enfileira(1);
+        fila.enfileira(2);
+        fila.enfileira(3);
+        System.out.println(fila.espiar());
+        System.out.println(fila.toString());
+    }
+}
